9f3bf4499d673f2c8249c286a9300b0fe02dc018
Bob Hennessey <hennessey.re@gmail.com>
2018-12-07 17:40:19 -0500

    deploy configs

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# deploys to app engine
+#
+# ./deploy.sh $environment $version_name
+# $environment : (dev|prod)
+# $tag_name : optional - app engine version name
+
+# environment
+promote=" --no-promote"
+if [ $1 == "dev" ]; then
+    promote=" --promote"
+fi
+
+# version
+version=$(date +"%Y%m%dt%H%M%S")
+if [ -n "$3"  ]; then
+    version=${3//./-}
+fi
+
+gcloud app deploy $promote --version $version app.yaml
