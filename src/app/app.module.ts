fc1553f32f645497663f1c345ca60315b4e8b5ff
Bob Hennessey <hennessey.re@gmail.com>
2018-12-08 03:05:40 -0500

    adding temp other component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AppRouterModule } from './/app-router.module';
+import { OtherComponent } from './other/other.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    OtherComponent
   ],
   imports: [
     BrowserModule,
