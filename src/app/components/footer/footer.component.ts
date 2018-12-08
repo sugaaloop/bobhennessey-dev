7c120cf8719787b26689ec6b39dafb1bf5b30e63
Bob Hennessey <hennessey.re@gmail.com>
2018-12-08 16:25:42 -0500

    sending social share outbound events to ga

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from 'src/app/services/analytics.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  socialLinks: any;
+
+  constructor(private analyticsService: AnalyticsService) { }
 
   ngOnInit() {
+    this.socialLinks = {
+      github: 'https://github.com/sugaaloop/bobhennessey-net',
+      linkedin: 'https://www.linkedin.com/in/bob-hennessey-1034467',
+    }
+  }
+
+  trackOutboundClick(url) {
+    this.analyticsService.trackOutboundLink(url);
   }
 
 }
